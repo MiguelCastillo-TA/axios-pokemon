da1506a37e4db4c69770e2b29a930549dfefb414
Miguel Castillo <castilma95@gmail.com>
2022-08-03 18:01:35 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemonData, setPokemonData] = useState([]);
+  const [favoritePokemon, setFavoritePokemon] = useState([])
+
+  useEffect(()=>{
+      axios.get('https://pokeapi.co/api/v2/pokemon')
+          .then(response=>{
+            console.log(response.data.results)
+            setPokemonData(response.data.results)
+          })
+  }, []); 
+
+  const handleAddToFavorite = (pokemon, index) => {
+    alert('you added pokemon to favorite')
+    console.log(pokemon)
+    const newPokemonData = pokemonData
+    newPokemonData.splice(index, 1)
+
+    setPokemonData(newPokemonData)
+    setFavoritePokemon([...favoritePokemon, pokemon])
+  }
+
+  const handleRemoveFromFavorites = (pokemon, index) => {
+    alert('removing from favorites')
+    const newPokemonFavorite = favoritePokemon
+    newPokemonFavorite.splice(index, 1)
+
+    setFavoritePokemon(newPokemonFavorite)
+    setPokemonData([...pokemonData, pokemon])
+  }
+
+
+  return(
+      <div style={{display:'flex', flexDirection: 'row'}}>
+        <div style={{flex: 1}}>
+          {pokemonData.map((pokemon, index) => {
+              return (
+              <p key={pokemon.name}>
+                {pokemon.name}
+                <button onClick={() => handleAddToFavorite(pokemon, index)}>
+                  add to favorites
+                </button>
+              </p> 
+              )
+          })}
+        </div>
+        <div  style={{flex: 1}}>
+          <p>These are my favorite pokemon</p>
+          {
+            favoritePokemon.map((pokemon, index) => {
+              return (
+                <div key={pokemon.name}>
+                  <p>
+                    {pokemon.name} 
+                  </p>
+                  <button onClick={() => handleRemoveFromFavorites(pokemon, index)}>
+                    remove from favorites
+                  </button>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+  )
 }
 
 export default App;
